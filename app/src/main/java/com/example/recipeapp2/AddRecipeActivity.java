a76465d9fa952540c32cdbd1441f154ce45aaab8
mahdijezzini <10219045@mu.edu.lb>
2024-05-23 12:37:04 +0300

    registration done

--- app/src/main/java/com/example/recipeapp2/AddRecipeActivity.java
+++ app/src/main/java/com/example/recipeapp2/AddRecipeActivity.java
@@ -1,10 +1,17 @@
 package com.example.recipeapp2;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ToggleButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AddRecipeActivity extends AppCompatActivity {
+
+    EditText recipeNameEditText,ingredientsEditText,stepsEditText;
+    ImageButton saveButton;
+    ToggleButton editToggleButton;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
